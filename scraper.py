bbf16ac18c116c3e24f4177398360f30e7122cae
ned <ned@me.com>
2021-11-17 19:53:29 +0100

    wtf

--- scraper.py
+++ scraper.py
@@ -27,10 +27,10 @@ PLEX_PROXY_IMG = os.environ["PLEX_PROXY_IMG"]
 SPOTIFY_CLIENT_ID = os.environ["SPOTIFY_CLIENT_ID"]
 SPOTIFY_CLIENT_SECRET = os.environ["SPOTIFY_CLIENT_SECRET"]
 SPOTIFY_REFRESH_TOKEN = os.environ["SPOTIFY_REFRESH_TOKEN"]
-TMDB_API_KEY=os.environ["TMDB_API_KEY"]
-IGDB_COOKIE=os.environ["IGDB_COOKIE"]
-IGDB_CLIENT_ID=os.environ["IGDB_CLIENT_ID"]
-IGDB_CLIENT_SECRET=os.environ["IGDB_CLIENT_SECRET"]
+TMDB_API_KEY = os.environ["TMDB_API_KEY"]
+IGDB_COOKIE = os.environ["IGDB_COOKIE"]
+IGDB_CLIENT_ID = os.environ["IGDB_CLIENT_ID"]
+IGDB_CLIENT_SECRET = os.environ["IGDB_CLIENT_SECRET"]
 
 LIMIT = 20
 IMG_WIDTH = '350'
@@ -52,7 +52,7 @@ def save_images(slug, ext, url, square=False):
         img_data = requests.get(url).content
         with open('static/img/' + slug + '.' + ext, 'wb') as f:
             f.write(img_data)
-        if square:    
+        if square:
             with open('static/img/' + slug + '.' + ext, 'r+b') as f:
                 with Image.open(f) as image:
                     square_image(image, 320).save('static/img/' + slug + '.' + ext, image.format)
@@ -75,153 +75,6 @@ def square_image(image: Image, length: int) -> Image:
             box=(required_loss / 2, 0, resized_image.size[0] - required_loss / 2, length))
         return resized_image
 
-# PLEX
-r = requests.get(url=PLEX_URL)
-j = r.json()
-rows = j['response']['data']['rows']
-
-## Movies
-data['movies'] = []
-movies = [row for row in rows if row['media_type'] == 'movie' and row['user'] == PLEX_USER]
-for movie in movies[:LIMIT]:
-    slug = slugify(movie['title'])
-    img_url = PLEX_PROXY_IMG + movie['thumb'] + '&width=' + IMG_WIDTH
-    save_images(slug, 'png', img_url)
-    guids = requests.get(PLEX_METADATA_URL + str(movie['rating_key'])).json()['response']['data']['guids']
-    data['movies'].append({
-        'title': movie['title'],
-        'guid': guids[1].split('//')[1],
-        'year': movie['year'],
-        'img': slug + '.png',
-        'img_webp': slug + '.webp',
-        'last_watch': movie['last_watch'],
-        'cinema': 'false',
-        'is_favorite': 'false'
-    })
-
-# Cinema
-d = feedparser.parse('https://letterboxd.com/n3d1117/rss/')
-lbxd_cinema_list = [item for item in d['entries'] if item['title'] == '🍿 Cinema'][0]
-cinema_movies_raw = re.findall("<li>(.*?)</li>", lbxd_cinema_list['summary'])
-cinema_movies = []
-for movie in cinema_movies_raw:
-    cinema_movies.append({ 'title': movie.split('">')[1].split('</a>')[0], 'link': movie.split('href="')[1].split('"')[0] })
-for movie in cinema_movies:
-    items = [item for item in d['entries'] if 'letterboxd_filmtitle' in item and item['letterboxd_filmtitle'] == movie['title']]
-    if len(items) > 0:
-        item = items[0]
-
-        slug = slugify(item['letterboxd_filmtitle'])
-        img_url = item['summary'].split('src="')[1].split('"')[0].replace('0-500-0-750', '0-230-0-345')
-        save_images(slug, 'jpg', img_url)
-
-        data['movies'].append({
-            'title': item['letterboxd_filmtitle'],
-            'guid': movie['link'],
-            'year': item['letterboxd_filmyear'],
-            'img': slug + '.jpg',
-            'img_webp': slug + '.webp',
-            'last_watch': int(datetime.datetime.strptime(item['letterboxd_watcheddate'], "%Y-%m-%d").timestamp()),
-            'cinema': 'true',
-            'is_favorite': 'false'
-        })
-
-# Fav Movies
-top_movies = requests.get(url="https://api.themoviedb.org/3/list/7112446?api_key=" + TMDB_API_KEY)
-top_movies_json = top_movies.json()
-for movie in top_movies_json['items']:
-    slug = slugify(movie['title'])
-    img_url = 'https://image.tmdb.org/t/p/w300' + movie['poster_path']
-    save_images(slug, 'jpg', img_url)
-
-    data['movies'].append({
-        'title': movie['title'],
-        'guid': str(movie['id']),
-        'year': movie['release_date'].split('-')[0],
-        'img': slug + '.jpg',
-        'img_webp': slug + '.webp',
-        'last_watch': int(datetime.datetime.strptime(movie['release_date'], "%Y-%m-%d").timestamp()),
-        'cinema': 'false',
-        'is_favorite': 'true'
-    })
-
-# TV Shows
-data['shows'] = []
-tv_shows = [row for row in rows if row['media_type'] == 'episode' and row['user'] == PLEX_USER]
-unique_shows = []
-unique_show_titles = []
-for show in tv_shows:
-    if show['grandparent_title'] not in unique_show_titles:
-        unique_show_titles.append(show['grandparent_title'])
-        unique_shows.append(show)
-for show in unique_shows[:LIMIT]:
-    slug = slugify(show['grandparent_title'])
-    img_url = PLEX_PROXY_IMG + show['thumb'] + '&width=' + IMG_WIDTH
-    save_images(slug, 'png', img_url)
-    guids = requests.get(PLEX_METADATA_URL + str(show['rating_key'])).json()['response']['data']['guids']
-
-    data['shows'].append({
-        'title': show['grandparent_title'],
-        'guid': guids[1].split('//')[1],
-        'ep': 'S' + str(show['parent_media_index']) + 'E' + str(show['media_index']),
-        'img': slug + '.png',
-        'img_webp': slug + '.webp',
-        'is_favorite': 'false'
-    })
-
-# Fav TV Shows
-top_shows = requests.get(url="https://api.themoviedb.org/3/list/7112447?api_key=" + TMDB_API_KEY)
-top_shows_json = top_shows.json()
-for show in top_shows_json['items']:
-    slug = slugify(show['name'])
-    img_url = 'https://image.tmdb.org/t/p/w300' + show['poster_path']
-    save_images(slug, 'jpg', img_url)
-
-    data['shows'].append({
-        'title': show['name'],
-        'guid': str(show['id']),
-        'ep': show['first_air_date'].split('-')[0],
-        'img': slug + '.jpg',
-        'img_webp': slug + '.webp',
-        'last_watch': int(datetime.datetime.strptime(show['first_air_date'], "%Y-%m-%d").timestamp()),
-        'is_favorite': 'true'
-    })
-
-# Books
-data['books'] = []
-OKU_URL='https://oku.club/api/collections/'
-READING='yjUNL'
-READ='xSQso'
-TO_READ='I0Ai5'
-FAVORITES='IPgqn'
-f = requests.get(OKU_URL + FAVORITES).json()
-d = requests.get(OKU_URL + READ).json()
-d2 = requests.get(OKU_URL + READING).json()
-d3 = requests.get(OKU_URL + TO_READ).json()
-for fav_book in f['books']:
-    slug = fav_book['slug']
-    save_images(slug, 'jpg', fav_book['thumbnail'])
-    data['books'].append({
-        'title': fav_book['title'],
-        'author': fav_book['authors'][0]['name'],
-        'url': 'https://oku.club/book/' + fav_book['slug'],
-        'img': slug + '.jpg',
-        'img_webp': slug + '.webp',
-        'is_favorite': 'true'
-    })
-for book in (d3['books'] + d2['books'] + d['books'])[:LIMIT]:
-    slug = book['slug']
-    save_images(slug, 'jpg', book['thumbnail'])
-    data['books'].append({
-        'title': book['title'],
-        'author': book['authors'][0]['name'],
-        'url': 'https://oku.club/book/' + book['slug'],
-        'img': slug + '.jpg',
-        'img_webp': slug + '.webp',
-        'is_favorite': 'false'
-    })
-
-
 # Spotify
 data['spotify'] = []
 SPOTIFY_TOKEN_URL='https://accounts.spotify.com/api/token'
@@ -242,26 +95,6 @@ for item in j['items']:
         'img': slug + '.jpeg',
         'img_webp': slug + '.webp'
     })
-
-
-# GitHub
-data['github'] = []
-GITHUB_URL = 'https://api.github.com/users/{}/repos?per_page=50'.format('n3d1117')
-exclude = ['CrackBot']
-j = requests.get(GITHUB_URL).json()
-d = sorted(j, key=lambda item: item['stargazers_count'], reverse=True)
-for project in [p for p in d if p['name'] not in exclude][:LIMIT]:
-    data['github'].append({
-        'name': project['name'],
-        'html_url': project['html_url'],
-        'description': project['description'],
-        'language': project['language'],
-        'stargazers_count': project['stargazers_count'],
-        'forks_count': project['forks_count'],
-    })
-
-# Videogames
-
 
 # Write data
 with open('data/scraper.json', 'w') as f:
