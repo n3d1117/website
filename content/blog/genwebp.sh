862db88eb5b64f0bf5d29c02b0d5e86456ffc202
ned <github@edoardo.fyi>
2023-01-05 18:36:50 +0100

    use built in hugo image processing for webp conversion

--- content/blog/genwebp.sh
+++
@@ -1,3 +0,0 @@
-find ./ -type f -name '*.jpg' -exec sh -c 'cwebp $1 -o "${1%.jpg}.webp"' _ {} \;
-find ./ -type f -name '*.jpeg' -exec sh -c 'cwebp $1 -o "${1%.jpeg}.webp"' _ {} \;
-find ./ -type f -name '*.png' -exec sh -c 'cwebp $1 -o "${1%.png}.webp"' _ {} \;
