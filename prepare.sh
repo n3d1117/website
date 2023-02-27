7ad9038dd66430cdaf504d214eec2ca0027dd98b
ned <github@edoardo.fyi>
2023-02-27 11:42:41 +0100

    bump libwebp to 1.3.0

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,9 @@
 # /bin/bash
 
 # Install cwebp
-wget -c https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.2.4-linux-x86-64.tar.gz
-tar -xvf libwebp-1.2.4-linux-x86-64.tar.gz
-export PATH=$PATH:"$(pwd)"/libwebp-1.2.4-linux-x86-64/bin
+wget -c https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.3.0-linux-x86-64.tar.gz
+tar -xvf libwebp-1.3.0-linux-x86-64.tar.gz
+export PATH=$PATH:"$(pwd)"/libwebp-1.3.0-linux-x86-64/bin
 
 # Scrape 
 python scraper.py
