f4f8d46ffc50985cec65fa1c690b9ea055b4eaac
ned <github@edoardo.fyi>
2025-07-12 22:12:16 +0200

    update webp

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 # /bin/bash
 
 # Install cwebp
-wget -c https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.4.0-linux-x86-64.tar.gz
-tar -xvf libwebp-1.4.0-linux-x86-64.tar.gz
-export PATH=$PATH:"$(pwd)"/libwebp-1.4.0-linux-x86-64/bin
+wget -c https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.6.0-linux-x86-64.tar.gz
+tar -xvf libwebp-1.6.0-linux-x86-64.tar.gz
+export PATH=$PATH:"$(pwd)"/libwebp-1.6.0-linux-x86-64/bin
 
 # Scrape content
 python scraper.py
